0cfd1c832c1bc8caa51235e08595bde891182d27
mutasem <mutasemms198@gmail.com>
2026-02-10 22:15:14 +0300

    notifications

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::USeBootstrap();
+        // Define morph map for polymorphic relationships
+        Relation::enforceMorphMap([
+            'user' => 'App\Models\User',
+            'driver' => 'App\Models\Driver',
+        ]);
+    
     }
 }
